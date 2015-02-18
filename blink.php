6ec6d9a9bffd4f4fb8c0ffb2d6066dbced7dc644
Igor Brandao <igorsca@gmail.com>
2015-02-18 02:44:07 -0200

    funcinando com innertHTML em ajax

--- blink.php
+++ blink.php
@@ -12,14 +12,21 @@ Pequeno Ajax que ira substituir o ledrpi.php
 <script>
 /*Pequeno script para mudar o status da lampada
 apenas alterando o style do background da div*/
+//Fazemos abaixo um innerHTML para chamar o bloco php com shell_exec
 window.onload = function blink(){
 	document.getElementById("liga").onclick = function(){
 		//alert("liga");//Apenas para debug
-		document.getElementById("chimage").style.backgroundPosition=("-146px 15px");
+		document.getElementById("chimage").style.backgroundPosition=("-146px 15px");	
+		document.getElementById("show").innerHTML="<?php 
+					$led=shell_exec('sudo /usr/share/nginx/www/led.py 24 1');
+					echo $led;?>"
 	}
 	document.getElementById("desliga").onclick = function(){
 		//alert("desliga");//Apenas para debug
 		document.getElementById("chimage").style.backgroundPosition=("15px 15px");
+		document.getElementById("show").innerHTML="<?php 
+					$led=shell_exec('sudo /usr/share/nginx/www/led.py 24 0');
+					echo $led;?>"
 	}
 }
 
@@ -43,6 +50,7 @@ div.lamp{
 <div class="lamp" id="chimage"></div>
 <button id="liga" type"button" onclick="blink()" >LIGA</button>
 <button id="desliga" type"button" onclick="blink()" >DESLIGA</button>
-
+<br />
+<span id="show"></span>
 </body>
 </html>
